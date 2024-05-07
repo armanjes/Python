f3f361b1efa83d5addeb7762893af9f53965d5ab
Arman <mdarmanjes@gmail.com>
2024-05-08 00:05:04 +0600

    Class & object

---
+++ OOP/class_01.py
@@ -0,0 +1,19 @@
+# 🎁 Class & object
+
+"""
+Python is an object oriented programming language. Everything in python is an object.
+"""
+
+#🚀 Create a Class
+# we create a class using the "class" keyword, followed by the name of the class.
+
+class myClass:
+    # myClass is the class name
+    x = 5
+    # we call variable as "properties" in OOP
+
+#🚀 Create an Object
+# we can use the class name to create objects and store it in a variable.
+
+myObj = myClass()
+print(myObj.x)
