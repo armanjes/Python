8e97818ce14aa5454e86a2d0d4331f9ef50ad8ff
Arman <mdarmanjes@gmail.com>
2024-05-15 16:02:38 +0600

    abstraction

--- OOP/class_08.py
+++ OOP/class_08.py
@@ -1,1 +1,14 @@
 # 🎁 Abstraction
+
+# 🚀 A class with one or more abstract method is known as abstract class. A method without body or method with only declaration is know as abstract method. We can't make object of an abstract class and we must override abstract methods in subclass.
+
+''' In Python we can't implement this feature directly. We have to use a module abc (abstract base class).
+'''
+
+from abc import ABC, abstractmethod
+
+class Vehical(ABC):
+    @abstractmethod
+    def go(self):
+        pass
+
