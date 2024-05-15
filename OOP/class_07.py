86e7e11aaf002c705fea36feac8261eb4d0a73bd
Arman <mdarmanjes@gmail.com>
2024-05-15 15:40:27 +0600

    method overriding

---
+++ OOP/class_07.py
@@ -0,0 +1,25 @@
+# 🎁 method overriding
+
+# 🚀 Method overriding allows a subclass to provide a specific implementation of a method that is already defined in its superclass. Which is similar like polymorphism. 
+
+class Animal:
+    def sound(self):
+        print("Generic animal sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+
+# Usage
+animal = Animal()
+animal.sound()  # Output: Generic animal sound
+
+dog = Dog()
+dog.sound()     # Output: Bark
+
+cat = Cat()
+cat.sound()     # Output: Meow
